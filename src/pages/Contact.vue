3aea9797c75e0cac13f2be8af159b9e66d3baf10
Shayne Quinton <shayne_quinton@hotmail.com>
2021-07-01 02:00:05 -0400

    finished first draft of contact me page
    Signed-off-by: Shayne Quinton <shayne_quinton@hotmail.com>

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,26 +1,32 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    Contact Me
-    <div>
-      <a target="_blank" :href="linkedIn.url"
-        ><q-icon size="lg" name="fab fa-linkedin"
-      /></a>
-      <a target="_blank" :href="github.url">
-        <q-icon size="lg" name="fab fa-github"
-      /></a>
-      <a :href="'mailto:' + email.url">
-        <q-icon size="lg" name="fas fa-envelope"
-      /></a>
+  <q-page>
+    <page-title title="Contact Me" />
+    <div class="shayne-content">
+      If you are interested in hiring me, or would like to chat here are some
+      ways you can do so:
+      <div style="text-align: center; margin-top: 24px">
+        <a class="shayne-icon" :href="'mailto:' + email.url">
+          <q-icon size="lg" name="fas fa-envelope"
+        /></a>
+        <a class="shayne-icon" target="_blank" :href="linkedIn.url"
+          ><q-icon size="lg" name="fab fa-linkedin"
+        /></a>
+        <a class="shayne-icon" target="_blank" :href="github.url">
+          <q-icon size="lg" name="fab fa-github"
+        /></a>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PageTitle from 'components/PageTitle.vue';
+import 'src/css/app.scss';
 
 export default defineComponent({
   name: 'Contact',
-  components: {},
+  components: { PageTitle },
   data: () => {
     return {
       linkedIn: { url: 'https://www.linkedin.com/in/shayne-quinton-809a5269/' },
