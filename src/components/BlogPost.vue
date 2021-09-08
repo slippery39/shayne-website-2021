7ef58c2b0931b792f660e45afadef0ee3b9a4bf3
Shayne Quinton <shayne_quinton@hotmail.com>
2021-09-07 23:10:09 -0400

    start of blog post page

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="padding-bottom: 100px">
+    <h3>{{ $route.params.id }}</h3>
+
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import DefaultBlog from 'src/data/Blog';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'BlogPost',
+});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -35,6 +35,10 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Contact.vue') }],
   },
+  {
+    path: '/blog', component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: ':id', component: () => import('components/BlogPost.vue') }]
+  },
   ...articleRoutes,
   // Always leave this as last one,
   // but you can also remove it
